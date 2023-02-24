86971c600fe3ee44cabab2b4629b4d787711f26c
Klinovitsky <klinovitsky@gmail.com>
2023-02-24 12:34:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Netology. First Program
+
+# Тройные кавычки позволяют создать одну строчку разделенную на несколько
+HELP = """
+help - print a reference
+add  - add a task into the list
+show - print all added tasks."""
+
+print(HELP)
+
+# Creation of the emty list
+tasks = []
+
+command = input("Input command: ")
+if command == "help":
+    print(HELP)
+elif command == "show":
+    print(tasks)
+elif command == "add":
+    task = input("Input the name of the task: ")
+    tasks.append(task)
+    print("The task added.")
+else:
+    print("\nUnknown command")
+
