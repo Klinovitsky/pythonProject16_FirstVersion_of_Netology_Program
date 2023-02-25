880fe624c4745a3d28d5db12e181d21b9a33b96a
Klinovitsky <klinovitsky@gmail.com>
2023-02-25 10:02:56 +0300

    Ver 7.2 (random)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Netology. First Program
+import random
 
 # Дата в Python не является типом данных, но мы можем импортировать модуль с именем datetime для работы с датами
 import datetime
@@ -15,7 +16,7 @@ print("Today: ", date_now, "Tomorrow will be: ", date_tomorrow)
 # Тройные кавычки позволяют создать одну строчку разделенную на несколько
 HELP = """
 help        - print a reference
-add         - add a task into the list
+add         - add a task into the list (enter the date "01012001" for random function)
 show/sh     - print all added tasks
 exit/quit/q - quit the program \n"""
 print(HELP)
@@ -24,6 +25,8 @@ print(HELP)
 today = list()
 tomorrow = []
 other = []
+random_list = ["Учится", "Любить", "Забоиться", "Оберегать", "Гладить"]
+random_scheduled = list()
 
 # Number of tasks the user can input = X = 3 pieces (pcs.)
 # x = 0
@@ -41,7 +44,7 @@ while run:
           print(HELP)
 
       elif command == "show" or command == "sh":
-          print("\nTODAY: ", today, "\nTomorrow: ", tomorrow, "\nOther: \n", other)
+          print("\nTODAY: ", today, "\nTomorrow: ", tomorrow, "\nOther: \n", other, "\nRandom: \n", random_scheduled)
 
       elif command == "add":
           # while run_data:
@@ -50,7 +53,7 @@ while run:
               data_temp = dt.datetime.strptime(date_str, '%d%m%Y').date()
               data_to_list = data_temp.strftime("%d%m%Y")
               print("You entered the date: ", data_to_list)
-              task = input("Input the name of the task: ")
+              task = input("Input the name of the task (fro random - no need): ")
 
               if data_to_list == date_now:
                   today.append(task)
@@ -58,6 +61,11 @@ while run:
               elif data_to_list == date_tomorrow:
                   tomorrow.append(task)
                   print("The task added to TOMORROW list: ", tomorrow)
+              # test random function
+              elif data_to_list == "01012001":
+                  task = random.choice(random_list)
+                  random_scheduled.append(task)
+                  print(f'Random task is: "{task}"')
               else:
                   other.append(task)
                   print("The task added to OTHER list: ", other)
